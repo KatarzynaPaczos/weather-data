51ed2b13b0e92e0c3a6f4c7f539e0e0c362b2ac3
KatarzynaPaczos <kat.paczos@gmail.com>
2025-10-09 11:15:33 +0200

    removed versions in requirements

--- airflow-start.sh
+++ airflow-start.sh
@@ -2,9 +2,9 @@
 set -e
 
 AIRFLOW_VERSION=3.0.6
+source ./venvs/airflow_${AIRFLOW_VERSION}/bin/activate
 export AIRFLOW_HOME=$(pwd)/airflow
 export PYTHONPATH=$AIRFLOW_HOME:$PYTHONPATH
-source ./venvs/airflow_${AIRFLOW_VERSION}/bin/activate
 
 # Launch Airflow and its components
 airflow standalone
